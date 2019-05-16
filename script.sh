5eda0da14b03d79039874ba84318b57e30757de9
jessik1020 <jmarinpe@unal.edu.co>
2019-05-15 21:45:59 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,16 +4,19 @@
 #Cambiar separador de la fecha 
 sed 's|/|-|g' data.csv > out.csv
 
-#Organizar fecha en formato YYYY-MM-DD
- awk  '{print gensub(/([0-9][0-9])-([0-9][0-9])-([0-9][0-9])/, "20\\3-\\2-\\1", 1)}' out.csv > out1.csv
- awk  '{print gensub(/([0-9])-[0-9])-([0-9][0-9])([0-9][0-9])/, "\\3-0\\2-0\\1", 1)}' out1.csv > out2.csv
- #Organizar campos nulos
- awk '{sub(/;N/, ";\\N"); print}' out2.csv > out3.csv
- awk '{gsub(/;;/,   ";\\N;"); print}' out3.csv > out4.csv
- awk '{gsub(/;\s/,   ";\\N"); print}' out4.csv > out5.csv
+#Cambiar último registro
+sed -e '12 s/\([0-9]\)-/0\1-/' -e '12 s/-\([0-9]\)-/-0\1-/' -e ' s/-\([0-9][0-9]\);/-20\1;/' out.csv > out1.csv
 
-#Cambiar minusculas por mayusculas 
- awk '{print toupper($0)}' out5.csv > out6.csv
+#Organizar fecha en formato YYYY-MM-DD
+ awk  '{print gensub(/([0-9][0-9])-([0-9][0-9])-([0-9][0-9][0-9][0-9])/, "\\3-\\2-\\1", 1)}' out1.csv > out2.csv
+ 
+ #Cambiar minusculas por mayusculas 
+ awk '{print toupper($0)}' out2.csv > out3.csv
+
+ #Organizar campos nulos
+ awk '{sub(/;N/, ";\\N"); print}' out3.csv > out4.csv
+ awk '{gsub(/;;/,   ";\\N;"); print}' out4.csv > out5.csv
+ awk '{gsub(/;\s/,   ";\\N"); print}' out5.csv > out6.csv
 
 #Cambiar separador ; por ,
 awk '{gsub(/;/, ","); print}' out6.csv > out7.csv
@@ -22,10 +25,10 @@ awk '{gsub(/;/, ","); print}' out6.csv > out7.csv
 awk  '{print gensub(/,/, ".", 4)}' out7.csv > out8.csv
 
 #Mostrar registros que no tienen campos nulos 
-sed '\:\N:d' out8.csv > out9.csv 
+#sed '\:\N:d' out8.csv > out9.csv 
 
 #Mostrar lab final
-cat out9.csv
+cat out8.csv
 
 #Borrar archivos
 rm out*
